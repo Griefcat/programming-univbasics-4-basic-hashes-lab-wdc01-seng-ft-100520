292153d9202237c9969ff85fc67d525359d0ad96
Anne Nardolilli <anardolilli@gmail.com>
2020-09-11 20:20:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,8 +5,8 @@ end
 def my_hash
  important_days ={
    :marriage => "February 22 2020",
-   :juans_bday => "August 14 1987"
- end 
+   :juans_bday => "August 14 1987",
+end 
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
